5b5273a6b5bf0ede9883da9d7ca5610bd34e5a90
= <=>
2014-06-06 00:55:38 +0200

    Revert "k"
    This reverts commit b9e271e2acacfd5834275721e051a8e11d31de0f.

--- spec/spec_user.rb
+++
@@ -1,6 +0,0 @@
-describe User do
-		it "should authenticate with matching username and password" do
-				user = Factory(:user, :email => 'frank@gmail.com', :password => 'secret')
-				User.find_by_email('frank@gmail.com').try(:authenticate, 'secret').should == user
-		end
-end
