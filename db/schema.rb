7806da357b787f56b2de5d1771bb06a9679e0c7c
= <=>
2014-06-06 02:45:02 +0200

    t

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 20140606000425) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
